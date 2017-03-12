06f7e26c6e58760ce34ae5ede8c0b5a0c16e8524
Milena Stancheva <mil.stancheva@gmail.com>
2017-03-12 10:54:32 +0200

    admin ui sidebar added

---
+++ HomeMadeFood/HomeMadeFood.Web/Areas/Admin/Models/IngredientViewModel.cs
@@ -0,0 +1,23 @@
+﻿using HomeMadeFood.Models;
+using HomeMadeFood.Models.Enums;
+using HomeMadeFood.Web.Common.Mapping;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeMadeFood.Web.Areas.Admin.Models
+{
+    public class IngredientViewModel : IMapFrom<Ingredient>
+    {
+        [MinLength(2)]
+        public string Name { get; set; }
+
+        public FoodType FoodType { get; set; }
+
+        public MeasuringUnitType MeasuringUnit { get; set; }
+
+        [Min(0)]
+        public decimal PricePerMeasuringUnit { get; set; }
+
+        [Min(0)]
+        public decimal Quantity { get; set; }
+    }
+}
